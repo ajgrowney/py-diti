ed2dd6d12f0a5efe0bf0252f0605e894e6dce158
Andrew Growney <ajgrowney@ku.edu>
2018-11-28 14:59:03 -0600

    Updated

--- THERM_IM_PREPROC.py
+++ THERM_IM_PREPROC.py
@@ -15,12 +15,13 @@ def rgbProcData(im, new_file_name):
     if channels != 3:
         return False
     new_r_im_data, new_g_im_data, new_b_im_data, im_r_arr, im_g_arr, im_b_arr = ([] for i in range(6))
-    n_r = 0
+    valid_pixs = []
     for row in range(height):
+        inner_pixs = []
         for column in range(width):
             pix_stdev = statistics.stdev(im[row,column])
             if pix_stdev > 8:
-                n_r += 1
+                inner_pixs.append(column)
                 b,g,r = im[row,column]
                 im_r_arr.append(r)
                 im_g_arr.append(g)
@@ -35,10 +36,15 @@ def rgbProcData(im, new_file_name):
                 new_r_im_data.append((0,0,0))
                 new_g_im_data.append((0,0,0))
                 new_b_im_data.append((0,0,0))
+        valid_pixs.append(inner_pixs)
     
     print "Blue: Skew=", skew(im_b_arr), " Kurtosis=", kurtosis(im_b_arr)
     print "Green Skew: ", skew(im_g_arr), " Kurtosis=", kurtosis(im_g_arr)
     print "Red Skew: ", skew(im_r_arr), " Kurtosis=", kurtosis(im_r_arr)
+    print "Num Valid Pix", len(valid_pixs)
+    row = 0
+    for l in valid_pixs:
+        
     ax1 = plt.subplot()
     ax1.hist([im_r_arr,im_g_arr,im_b_arr], bins=np.arange(256))
     ax1.set_ylim(0,1500)
@@ -46,51 +52,26 @@ def rgbProcData(im, new_file_name):
     #plt.hist(im_g_arr, bins=np.arange(256))
     #plt.hist(im_b_arr, bins=np.arange(256))
 
-    plt.show()
 
-    # for pix in im.getdata():
-    #     b,g,r = pix
-    #     pix_stdev = statistics.stdev([r,g,b])
-    #     if pix_stdev > 8:
-    #         n_r += 1
-    #         im_r_arr.append(r)
-    #         im_g_arr.append(g)
-    #         im_b_arr.append(b)
-    #         new_r_im_data.append((r,0,0))
-    #         new_g_im_data.append((0,g,0))
-    #         new_b_im_data.append((0,0,b))
-    #     else:
-    #         new_r_im_data.append((0,0,0))
-    #         new_g_im_data.append((0,0,0))
-    #         new_b_im_data.append((0,0,0))
 
-    # img_r = Image.new('RGB', (width, height))
-    # img_g = Image.new('RGB', (width, height))
-    # img_b = Image.new('RGB', (width, height))
-    # img_r.putdata(new_r_im_data)
-    # img_g.putdata(new_g_im_data)
-    # img_b.putdata(new_b_im_data)
-
-    # print skew(im_r_arr)
-    # print skew(im_g_arr)
-    # print skew(im_b_arr)
-    # # img_r.save('./testing_results/r_'+new_file_name)
-    # # img_g.save('./testing_results/g_'+new_file_name)
-    # # img_b.save('./testing_results/b_'+new_file_name)
-    # fig = plt.figure()
-    # ax1 = fig.add_subplot(1,3,1)
-    # ax2 = fig.add_subplot(1,3,2)
-    # ax3 = fig.add_subplot(1,3,3)
-    # ax1.imshow(img_r)
-    # ax2.imshow(img_g)
-    # ax3.imshow(img_b)
-    # plt.show()
 
 # Description:
 # @Param im {PIL.Image Object} - image object converted to grayscale for analysis
 def grayProcData(im):
-    plt.imshow(im)
+        # Edge Detection
+
+    sobel_x = cv2.Sobel(im, cv2.CV_64F,1,0,ksize=5)
+    sobel_y = cv2.Sobel(im, cv2.CV_64F,0,1,ksize=5)
+    laplacian = cv2.Laplacian(im, cv2.CV_64F)
+    canny = cv2.Canny(im,300,200)
+    plt.subplot(2,2,1), plt.imshow(sobel_x,cmap='gray')
+    plt.subplot(2,2,2), plt.imshow(sobel_y, cmap='gray')
+    plt.subplot(2,2,3), plt.imshow(laplacian)
+    plt.subplot(2,2,4), plt.imshow(canny)
     plt.show()
+    #plt.imshow(im)
+    #plt.show()
+    return
 
 # Description:
 # @Param: image{'*.jpg','*.png'} - image to be analyzed and processed into classifier data
