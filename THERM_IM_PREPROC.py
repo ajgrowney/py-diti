01fd3db942257954983bc6c0d33c4ccb82b04ead
Andrew Growney <ajgrowney@gmail.com>
2019-02-04 16:09:12 -0600

    Update: plotting statistical data

--- THERM_IM_PREPROC.py
+++ THERM_IM_PREPROC.py
@@ -6,6 +6,7 @@ import cv2
 from scipy.misc import imread
 from scipy.stats import kurtosis, skew
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import FastICA, PCA
 
 class NumpyEncoder(json.JSONEncoder):
@@ -63,7 +64,7 @@ def rgbProcData(im, file_name):
 
     results["right_mean"] = right_mean
     results["left_mean"] = left_mean
-    results["image_mean"] = image_mean 
+    results["image_mean"] = image_mean
 
     im_sub = np.subtract(right_flipped,left_im)
     im_sub2 = np.subtract(left_im,right_flipped)
@@ -81,7 +82,7 @@ def rgbProcData(im, file_name):
     # cv2.imwrite('./testing_results/imsub_g.jpg',sub_g)
     # cv2.imwrite('./testing_results/imsub_r.jpg',sub_r)
 
-    # # Image Left, Right Segemented Files
+    # # Image Left, Right Segemented Filesskew
     # cv2.imwrite('./testing_results/left.jpg',left_im)
     # cv2.imwrite('./testing_results/right.jpg',right_flipped)
 
@@ -92,7 +93,7 @@ def rgbProcData(im, file_name):
     results["b_skew"] = skew(b)
     results["g_skew"] = skew(g)
     results["r_skew"] = skew(r)
-    
+
     results["b_kurtosis"] = kurtosis(b)
     results["g_kurtosis"] = kurtosis(g)
     results["r_kurtosis"] = kurtosis(r)
@@ -122,7 +123,7 @@ def grayProcData(im):
     canny = cv2.Canny(im,300,200)
     plt.subplot(2,2,1), plt.imshow(im,cmap='gray')
     plt.subplot(2,2,2), plt.imshow(sobel_y, cmap='gray')
-    plt.subplot(2,2,3), plt.imshow(laplacian)
+    plt.subplot(2,2,3), plt.imImagesshow(laplacian)
     plt.subplot(2,2,4), plt.imshow(canny)
     plt.show()
     return
@@ -132,8 +133,8 @@ def grayProcData(im):
 # @Param: new_file_name {String} -
 
 def preProcImage(image):
-    f_im = cv2.imread('./Cancer/'+image+'A2BA-f.jpg',1)
-    fc_im = cv2.imread('./Cancer/'+image+'A2BA-fc.jpg',1)
+    f_im = cv2.imread('./Images/'+image+'A2BA-f.jpg',1)
+    fc_im = cv2.imread('./Images/'+image+'A2BA-fc.jpg',1)
 
     test_f_im = filterBackground(f_im,image,'A2BA-f')
     test_fc_im = filterBackground(fc_im,image,'A2BA-fc')
@@ -145,9 +146,9 @@ def preProcImage(image):
     # cv2.imwrite('./tf_gray.jpg',test_f_imgray)
     # cv2.imwrite('./tfc_gray.jpg',test_fc_imgray)
 
-    # # Utilize the RGB Colorspace to analyze the image and pull statistics
-    # # rgb_proc_f = rgbProcData(f_im, image)
-    # # rgb_proc_fc = rgbProcData(fc_im,image)
+    # Utilize the RGB Colorspace to analyze the image and pull statistics
+    rgb_proc_f = rgbProcData(f_im, image)
+    rgb_proc_fc = rgbProcData(fc_im,image)
 
     # # fname = './testing_results/json_results/'+image+'_f_data.txt'
     # # fcname = './testing_results/json_results/'+image+'_fc_data.txt'
@@ -157,20 +158,109 @@ def preProcImage(image):
 
     # grayProcData(test_f_imgray)
     # grayProcData(test_fc_imgray)
-    # return rgb_proc_f, rgb_proc_fc
+    return rgb_proc_f, rgb_proc_fc
+def compileHistogramResults(single_im_f, single_im_fc, current_results):
+    current_results["skews"]["r_fc"].append(single_im_fc['r_skew'])
+    current_results["skews"]["r_f"].append(single_im_f['r_skew'])
+    current_results["skews"]["b_fc"].append(single_im_fc['b_skew'])
+    current_results["skews"]["b_f"].append(single_im_f['b_skew'])
+    current_results["skews"]["g_fc"].append(single_im_fc['g_skew'])
+    current_results["skews"]["g_f"].append(single_im_f['g_skew'])
+    current_results["kurtosis"]["r_fc"].append(single_im_fc['r_kurtosis'])
+    current_results["kurtosis"]["r_f"].append(single_im_f['r_kurtosis'])
+    current_results["kurtosis"]["b_fc"].append(single_im_fc['b_kurtosis'])
+    current_results["kurtosis"]["b_f"].append(single_im_f['b_kurtosis'])
+    current_results["kurtosis"]["g_fc"].append(single_im_fc['g_kurtosis'])
+    current_results["kurtosis"]["g_f"].append(single_im_f['g_kurtosis'])
+    current_results["means"]["total_f"].append(single_im_f['image_mean'])
+    current_results["means"]["total_fc"].append(single_im_fc['image_mean'])
+    return current_results
+
+def displayResults(results):
+    fig = plt.figure()
+
+    ax = fig.add_subplot(2,2,1)
+    ax.set_title("Skew Data")
+    skew_data = [results["skews"]["r_f"], results["skews"]["r_fc"], results["skews"]["g_f"], results["skews"]["g_fc"], results["skews"]["b_f"], results["skews"]["b_fc"]]
+    colors = ['red', 'crimson','green', 'yellow','blue', 'skyblue']
+    n, bins, patches = plt.hist(skew_data, 10, color=colors)
+
+    ax = fig.add_subplot(2,2,2)
+    ax.set_title("Kurtosis Data")
+    kurt_data = [results["kurtosis"]["r_f"], results["kurtosis"]["r_fc"], results["kurtosis"]["g_f"], results["kurtosis"]["g_fc"], results["kurtosis"]["b_f"], results["kurtosis"]["b_fc"]]
+    colors = ['red', 'crimson','green', 'yellow','blue', 'skyblue']
+    n, bins, patches = plt.hist(kurt_data, 10, color=colors)
+
+    ax = fig.add_subplot(2,2,3, projection='3d')
+    ax.set_title("Image Means - F")
+    xf_vals = list(x[0] for x in results["means"]['total_f'])
+    yf_vals = list(x[1] for x in results["means"]['total_f'])
+    zf_vals = list(x[2] for x in results["means"]['total_f'])
+
+    ax.scatter(xf_vals, yf_vals, zf_vals)
+    ax.set_xlabel('R Values')
+    ax.set_ylabel('G Values')
+    ax.set_zlabel('B Values')
+
+    ax = fig.add_subplot(2,2,4, projection='3d')
+    ax.set_title("Image Means - FC")
+    xfc_vals = list(x[0] for x in results["means"]['total_fc'])
+    yfc_vals = list(x[1] for x in results["means"]['total_fc'])
+    zfc_vals = list(x[2] for x in results["means"]['total_fc'])
+
+    ax.scatter(xfc_vals, yfc_vals, zfc_vals)
+    ax.set_xlabel('R Values')
+    ax.set_ylabel('G Values')
+    ax.set_zlabel('B Values')
+    plt.show()
 
 def main():
     arg_len = len(sys.argv)
     if arg_len == 2 and sys.argv[1] == "frontDataAll":
+        # Results Object
+        total_results = {
+            "skews": {
+                "r_f": [], "r_fc": [], "g_f": [], "g_fc": [], "b_f": [], "b_fc": []
+            },
+            "kurtosis": {
+                "r_f": [], "r_fc": [], "g_f": [], "g_fc": [], "b_f": [], "b_fc": []
+            },
+            "means": {
+                "total_f": [], "total_fc": [], "left_f": [], "left_fc": [], "right_f": [], "right_fc": []
+            }
+        }
+
         patlist = open("patients_cancer.txt", "r").readlines()
         patlist[0] = patlist[0][1:]
         for pat in patlist[:-1]:
             pat = pat.strip().replace('\r','')
             pat = pat[1::2]
             pat = pat[:-1]
-            preProcImage(pat)
+            pat_res_f, pat_res_fc = preProcImage(pat)
+            total_results = compileHistogramResults(pat_res_f, pat_res_fc, total_results)
+        displayResults(total_results)
+
     elif arg_len == 2:
-        print preProcImage("FloMar310310")
+        print preProcImage("ArmAna240313")
+
+    elif arg_len == 1:
+        test_arr = ["ArmAna240313", "AcoAlm221112", "AguCat100309", "AisMic030513", "AndPet151112"]
+        total_results = {
+            "skews": {
+                "r_f": [], "r_fc": [], "g_f": [], "g_fc": [], "b_f": [], "b_fc": []
+            },
+            "kurtosis": {
+                "r_f": [], "r_fc": [], "g_f": [], "g_fc": [], "b_f": [], "b_fc": []
+            },
+            "means": {
+                "total_f": [], "total_fc": [], "left_f": [], "left_fc": [], "right_f": [], "right_fc": []
+            }
+        }
+        for pat in test_arr:
+            pat_res_f, pat_res_fc = preProcImage(pat)
+            total_results = compileHistogramResults(pat_res_f, pat_res_fc, total_results)
+
+        displayResults(total_results)
 
 if __name__ == '__main__':
     main()
